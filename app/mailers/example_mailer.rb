a0c2a121883c8f7d3567823677533afa74d06b73
sunil mandiwal <sunil@fixnix.co>
2016-10-14 18:30:32 +0530

    mail is going its showing in terminal but on mail not getting confirmation mail

---
+++ app/mailers/example_mailer.rb
@@ -0,0 +1,9 @@
+class ExampleMailer < ApplicationMailer
+ default from: "mandiwalsunil91@gmail.com"
+
+def sample_email(user)
+    @user = user
+    mail(to: @user.email, subject: 'Sample Email')
+  end
+
+end
