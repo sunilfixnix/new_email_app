63a3605adc10e98a60cffd5b7020dacc31d9f3a4
sunil mandiwal <sunil@fixnix.co>
2016-10-15 17:10:08 +0530

    now mail is reaching to mail id but mailcatcher is not working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
 
- ExampleMailer.sample_email(@user).deliver
+         ExampleMailer.sample_email(@user).deliver
 
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
